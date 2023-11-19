a73cc7d2d85d9c010398d90d8a95c815dd723020
akfrdma0125 <nogain1215@gmail.com>
2023-11-19 22:52:40 +0900

    feat: user-reducer를 이용한 간단한 계산 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,50 +1,48 @@
-import React, {createContext, useContext} from 'react';
-import './style.css';
-
-const themeDefault = {border: '10px solid red'}
-const themeContext = createContext(themeDefault); //context의 default값
-//이점: enum클래스 같은 전역 적용
+import React, {useReducer, useState} from 'react';
 
 export default function App() {
-  const theme = useContext(themeContext); 
-  console.log(theme); //themeDefault 값 리턴
-  return (
-    <themeContext.Provider value={{border: '10px solid blue'}}>
-      <div className='root' style={theme}>
-        <h1>Hello World!</h1>
-        <Sub1 />
-      </div>
-    </themeContext.Provider>
-  );
+  //dispatch -> reducer -> action (기존은 바로 action)
+  //dispatch: 청구 직원
+  //1st parameter: reducer, 2nd param: inital value
+  const [count, countDispatch] = useReducer(countReducer, 0);
+  const [number, setNumber] = useState(1);
 
-  function Sub1(){
-    const theme = useContext(themeContext);
-    return(
-      <themeContext.Provider value = {{border: '10px solid green'}}>
-        <div style={theme}>
-          <h1>Sub1</h1>
-          <Sub2/>
-        </div>
-      </themeContext.Provider>
-    )
+  //reducer: 장부를 기록하는 회계 직원
+  function countReducer(oldCount, action){
+    if (action.type === 'UP') {
+      return oldCount + action.number;
+    } else if (action.type === 'DOWN'){
+      return oldCount - action.number;
+    } else if (action.type === 'RESET') {
+      return 0;
+    }
   }
 
-  function Sub2(){
-    const theme = useContext(themeContext);
-    return(
-      <div style={theme}>
-        <h1>Sub2</h1>
-        <Sub3/>
-      </div>
-    )
+  function down(){
+    countDispatch({type: 'DOWN', number: number});
   }
 
-  function Sub3(){
-    const theme = useContext(themeContext);
-    return(
-      <div style={theme}>
-        <h1>Sub3</h1>
-      </div>
-    )
+
+  function reset(){
+    countDispatch({type: 'RESET', number: number});
   }
+
+
+  function up(){
+    countDispatch({type: 'UP', number: number});
+  }
+  
+  function changeNumber(event){
+    setNumber(Number(event.target.value));
+  }
+
+  return(
+    <div>
+      <input type='button' value="-" onClick={down}/>
+      <input type='button' value="0" onClick={reset}/>
+      <input type='button' value="+" onClick={up}/>
+      <input type='text' value={number} onChange={changeNumber}/>
+      <span>{count}</span>
+    </div>
+  )
 }
